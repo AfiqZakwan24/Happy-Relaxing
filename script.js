ceaa85c24703ced4676d1a8c54fc149962fa65c9
Afiq Zakwan <67037262+AfiqZakwan24@users.noreply.github.com>
2022-01-16 22:56:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function LivePerf() {
+    window.location.href="LivePerformance.html";
+}
+
+function SDate() {
+    window.location.href="slotDate.html";
+}
+
+function SInfo() {
+    window.location.href="slotInfo.html";
+}
+
+function SVerify() {
+    window.location.href="slotVerify.html";
+}
+
+function HomePage() {
+    alert("Your record have been saved.");
+    window.location.href="index.html";
+}
+
+function tac() {
+    alert("Your TAC number is 123456");
+}
+
+function reminder() {
+    alert("You have set your reminder. See you soon. :)");
+}
+
+function startLive(){
+    window.location.href="LivePerf-music.html";
+}
